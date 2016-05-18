883cb35f6e01a573b7a04b54f90d217003ba9576
Andreas Mayer <andisspam@gmail.com>
2016-05-18 19:03:05 +0200

    add amssymb to latex preamble to allow bb fonts

--- doc/conf.py
+++ doc/conf.py
@@ -216,7 +216,7 @@ latex_elements = {
 #'pointsize': '10pt',
 
 # Additional stuff for the LaTeX preamble.
-#'preamble': '',
+'preamble': '\usepackage{amssymb}',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
